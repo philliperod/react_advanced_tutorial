eb5fa19770464c05141055a2b385ec7bcf6b6274
Phillipe Rodriguez <philliperod@gmail.com>
2020-10-15 18:50:57 -0500

    useState array example: setting up a destructured object for useState then accessing props from the data component

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import { data } from '../../../data';
+import {data} from '../../../data';
 
 const UseStateArray = () => {
+  const [people, setPeople] = React.useState(data);
   return <h2>useState array example</h2>;
 };
+// 1) first, let's setup the useState
+// 2) the value is 'people' and the function is 'setPeople'
+// 3) another way to invoke useState if not importing React is using React.useState (optional)
+// 4) in this example, we'll set the useState default value as data that's imported from data.js
 
 export default UseStateArray;
