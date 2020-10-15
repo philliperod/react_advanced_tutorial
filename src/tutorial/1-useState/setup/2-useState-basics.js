9214b4062ada27e3e76b49dbf7cfaea2e7568780
Phillipe Rodriguez <philliperod@gmail.com>
2020-10-15 17:05:16 -0500

    created a button that has a function that will change useState value once clicked; also has an if statement that will change the value back and forth

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -8,26 +8,25 @@ const UseStateBasics = () => {
   // console.log(value, handleFunction);
 
   const [text, setText] = useState('UseState Random Title');
+  const handleClick = () => {
+    if (text === 'UseState Random Title') {
+      setText('New Title Now');
+    } else {
+      setText('UseState Random Title');
+    }
+  };
 
   return (
     <React.Fragment>
       <h2>{text}</h2>
+      <button className="btn" onClick={handleClick}>
+        Change title
+      </button>
     </React.Fragment>
   );
 };
-// 1) UseState is a function
-// 2) it is a named import from React; must use curly braces in the import
-// 3) useState function returns as an array which first value (state value) is undefined and the second value is a function
-// 4) the function will control the state value
-// 5) when invoking the useState(), you must set a default value which can be anything
-// 6) for example, we are going to setup a value and function to see how it is access
-// 7) const value will be the default value; the brackets selects the index of that value
-// 8) const handleFunction will be the function that controls the value
-
-// 9) you can use destructuring and set it for useState
-// 10) you will use brackets instead of curly braces
-// 11) the function will have a standard 'set' before the name of the function; should match the value set in the bracket
-// 12) inside the parameters of the useState is where you can set the default value
-// 13) then using React.Fragment again, inside the html tags you will use {valuu} inbetween
+// 1) now we will create a button that will handle the useState 'setText' function
+// 2) reminder that the function inside the HTML tag will need to be in curly braces to access it
+// 3) next is to create an if statement to have the title change back and forth
 
 export default UseStateBasics;
