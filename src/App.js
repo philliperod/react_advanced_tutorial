a1ba74325f82a70cd95745425aadbc103c6cc871
Phillipe Rodriguez <philliperod@gmail.com>
2020-10-15 14:35:50 -0500

    first exercise: UseState Error; imported the component into app.js to practice the exercise; you can name the import statement any name you choose; you must use that specific name inside the component in order to use that imported component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Setup from './tutorial/1-useState/setup/1-error-example';
+// reminder that you can name the import any name you choose
+// you just need to make sure you match the name inside your component to use it
+
 function App() {
   return (
-    <div className='container'>
-      <h2>Advanced Tutorial</h2>
+    <div className="container">
+      <Setup />
     </div>
   );
 }
