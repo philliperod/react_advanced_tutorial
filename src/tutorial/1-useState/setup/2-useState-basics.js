a2931ea6ef261b3a472f05dde9f580c162594643
Phillipe Rodriguez <philliperod@gmail.com>
2020-10-15 15:44:03 -0500

    useState(): console.log the useState function to see what its output is which shows an array; first value is the state value and second is the function that controls it

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,13 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
+// in this example, using UseState to change the title back and forth
 
 const UseStateBasics = () => {
+  console.log(useState());
+
   return <h2>useState basic example</h2>;
 };
+// 1) UseState is a function
+// 2) it is a named import from React; must use curly braces in the import
+// 3)
 
 export default UseStateBasics;
