c9a353ae464f4ed48dd239e655dc350b4d0b00a8
Phillipe Rodriguez <philliperod@gmail.com>
2020-10-15 15:10:38 -0500

    UseState Basics: attempted to change the title by changing the value within the handleClick function but does not work; it will console.log the new value, but it will not render the original title; thus proceeding to use UseState

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -4,9 +4,16 @@ import React from 'react';
 
 const ErrorExample = () => {
   let title = 'This Title';
+  const handleClick = () => {
+    title = 'New Title';
+    console.log(title);
+  };
   return (
     <React.Fragment>
       <h2>{title}</h2>
+      <button type="button" className="btn" onClick={handleClick}>
+        Change title
+      </button>
     </React.Fragment>
   );
 };
@@ -15,4 +22,8 @@ const ErrorExample = () => {
 // 2) Use React.Fragment
 // React.Fragment: lets you group a list of children without adding extra nodes to DOM
 // 3) insert the variable in curly braces within HTML tags to access it
+// 4) setup button to handle changing the title
+// 5) create a function for the button to change the title
+// 6) in order to change the title, you must use UseState
+// 7)
 export default ErrorExample;
