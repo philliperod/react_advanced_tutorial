95bc25a93d15edbe342e6ed31d1c530e60c33990
Phillipe Rodriguez <philliperod@gmail.com>
2020-10-15 14:59:42 -0500

    first exercise: channging the title via button click; setup variable, used React.Fragment which contains the HTML tags that has access to the variable that is within curly braces

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Setup from './tutorial/1-useState/setup/1-error-example';
-import Final from './tutorial/1-useState/final/1-error-example';
+// import Final from './tutorial/1-useState/final/1-error-example';
 // reminder that you can name the import any name you choose
 // you just need to make sure you match the name inside your component to use it
 
@@ -8,7 +8,6 @@ function App() {
   return (
     <div className="container">
       <Setup />
-      <Final />
     </div>
   );
 }
--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
+// in this example, we want to change the title via button click
+
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  let title = 'This Title';
+  return (
+    <React.Fragment>
+      <h2>{title}</h2>
+    </React.Fragment>
+  );
 };
 
+// 1) set a variable for a title
+// 2) Use React.Fragment
+// React.Fragment: lets you group a list of children without adding extra nodes to DOM
+// 3) insert the variable in curly braces within HTML tags to access it
 export default ErrorExample;
