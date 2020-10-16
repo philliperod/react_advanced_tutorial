75e5ee2d33faa3dc2ed69bd6ecab0818de4c7af7
Phillipe Rodriguez <philliperod@gmail.com>
2020-10-16 08:35:36 -0500

    created destructured array; {data} prop id is set as a key attribute in the div and {data} prop name is inbetween the h2 tags to display onto the browser inside the div

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -8,18 +8,18 @@ const UseStateArray = () => {
   return (
     <>
       {people.map((person) => {
-        console.log(person);
-        return 'hello';
+        const {id, name} = person;
+        return (
+          <div key={id} className="item">
+            <h4>{name}</h4>
+          </div>
+        );
       })}
     </>
   );
 };
-// 1) first, let's setup the useState
-// 2) the value is 'people' and the function is 'setPeople'
-// 3) another way to invoke useState if not importing React is using React.useState (optional)
-// 4) in this example, we'll set the useState default value as data that's imported from data.js"
-// 5) now to return the hook which you can put inside React.Fragment (optional is using just the tags with naming it React.Fragment)
-// 6) Next, let's focus on the return which you want to iterate over your {data}
-// 7) You will use the map() method
+// 8) next, we'll destructure the array within the map() method
+// 9) and return a <div> that has a key attribute with the {data} prop id
+// 10) the <h4> will contain the {data} prop name
 
 export default UseStateArray;
