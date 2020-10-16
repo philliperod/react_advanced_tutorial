cefe44a9dd7a4cbf192fad08a5585b113342e999
Phillipe Rodriguez <philliperod@gmail.com>
2020-10-16 12:48:47 -0500

    UseState Object Example: created a destructured object assigned to a hook which will pass object, variable, or array

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,20 @@
 import React, { useState } from 'react';
 
+// UseState Object Example
+
 const UseStateObject = () => {
+  const [person, setPerson] = useState({
+    name: 'Phil',
+    age: 38,
+    message: 'Learning React'
+  });
+  console.log(person);
+
   return <h2>useState object example</h2>;
 };
+// 1) create a hook
+// 2) person is state value, setPerson is the function that will access that value
+// 3) it will equal to the useState in which it will pass an object, variable, or array
+// 4)
 
 export default UseStateObject;
