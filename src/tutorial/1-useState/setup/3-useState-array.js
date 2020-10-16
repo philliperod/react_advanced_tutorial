8898289f6734c56ae54d8daf31420b6c0aa72950
Phillipe Rodriguez <philliperod@gmail.com>
2020-10-16 08:24:25 -0500

    setup a return that uses the map() method to iterate over the {data} array from data component; console.log to show each prop and return 'hello' onto the browser

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import {data} from '../../../data';
 
+// UseState: Array Example
+
 const UseStateArray = () => {
   const [people, setPeople] = React.useState(data);
-  return <h2>useState array example</h2>;
+  return (
+    <>
+      {people.map((person) => {
+        console.log(person);
+        return 'hello';
+      })}
+    </>
+  );
 };
 // 1) first, let's setup the useState
 // 2) the value is 'people' and the function is 'setPeople'
 // 3) another way to invoke useState if not importing React is using React.useState (optional)
-// 4) in this example, we'll set the useState default value as data that's imported from data.js
+// 4) in this example, we'll set the useState default value as data that's imported from data.js"
+// 5) now to return the hook which you can put inside React.Fragment (optional is using just the tags with naming it React.Fragment)
+// 6) Next, let's focus on the return which you want to iterate over your {data}
+// 7) You will use the map() method
 
 export default UseStateArray;
