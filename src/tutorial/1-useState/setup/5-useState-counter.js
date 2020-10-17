0017c70072f390204d248e57b0a522470816a087
Phillipe Rodriguez <philliperod@gmail.com>
2020-10-17 17:56:26 -0500

    refactor the complexIncrease function to have the state function setValue with a function in the parameter; this will take the current value and add onto it before the delay update; rather than the previous function that would take the same value and add onto it

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -11,9 +11,18 @@ const UseStateCounter = () => {
 
   const complexIncrease = () => {
     setTimeout(() => {
-      setValue(value + 1);
+      // setValue(value + 1);
+      setValue((previousState) => {
+        return previousState + 1;
+      });
     }, 2000);
   };
+  // new approach is using another function within the parameter of the setValue instead of a direct state value
+  // the function will get the current value rather than the old state value
+  // the function will need a parameter that accepts the current state value
+  // when you click on the complex button multiple times, it will add with each click and add it one by one after the delay
+  // so setValue(value + 1) will get the same value and add plus one
+  // setValue((previousState) => {return previousState + 1}) will get the current value then add plus one right before the update
 
   return (
     <>
