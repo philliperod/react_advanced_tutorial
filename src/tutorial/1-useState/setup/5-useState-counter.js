e344144301f0e0c0d1f72b3e8f57c03e382f38eb
Phillipe Rodriguez <philliperod@gmail.com>
2020-10-17 07:50:50 -0500

    simple counter: created a <section> that contains heading, counter number, buttons that increase/decrease/reset the counter value; use both inline and reference functions for the buttons to change that value

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,35 @@
 import React, { useState } from 'react';
 
+// Simple Counter
+// 1) created a heading with a counter value and buttons to increase/descrease/reset that value
+// 2) created an inline function for the onClick handler for the increase/decrease button
+// 3) create a reference function for the reset button
+// side note: this is repetition to show that you can choose inline or reference functions
+
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [value, setValue] = useState(0);
+
+  const reset = () => {
+    setValue(0);
+  };
+
+  return (
+    <>
+      <section style={{ margin: '4rem 0' }}>
+        <h2>regular counter</h2>
+        <h1>{value}</h1>
+        <button className="btn" onClick={() => setValue(value + 1)}>
+          Increase
+        </button>
+        <button className="btn" onClick={() => setValue(value - 1)}>
+          Decrease
+        </button>
+        <button className="btn" onClick={reset}>
+          Reset
+        </button>
+      </section>
+    </>
+  );
 };
 
 export default UseStateCounter;
