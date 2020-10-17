ec69be0a31685929f801568718388a36cf155926
Phillipe Rodriguez <philliperod@gmail.com>
2020-10-17 07:01:07 -0500

    created a changeMessage function that will change the value of the value of prop message in the hook via button click

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -10,11 +10,29 @@ const UseStateObject = () => {
   });
   console.log(person);
 
-  return <h2>useState object example</h2>;
+  const changeMessage = () => {
+    setPerson({ message: 'Learned React' });
+  };
+
+  return (
+    <>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.message}</h3>
+      <button className="btn" onClick={changeMessage}>
+        Change Message
+      </button>
+    </>
+  );
 };
 // 1) create a hook
-// 2) person is state value, setPerson is the function that will access that value
+// 2) person is state value, setPerson is the function that will control that value
 // 3) it will equal to the useState in which it will pass an object, variable, or array
-// 4)
+// 4) what will happen if you change the state value?
+// 5) first, set the return in the React.Fragment (shorthand)
+// 6) return a <h3> with each prop within curly braces inbetween the tags
+// 7) next, create a button that will change those values in the array
+// 8) set the onClick with a function (changeMessage) that will be invoked once clicked
+// 9) inside the changeMessage function, you will invoke the state function (setPerson)
 
 export default UseStateObject;
