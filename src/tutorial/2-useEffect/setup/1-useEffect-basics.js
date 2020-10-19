142f22390886eb41a282168380d51dba69e900c6
Phillipe Rodriguez <philliperod@gmail.com>
2020-10-19 14:36:28 -0500

    created both useState and useEffect; created a button that once clicked, it will run the state function (setValue); after the state function is invoked and rendered, useEffect will run

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,13 +1,34 @@
 import React, { useState, useEffect } from 'react';
 
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+  useEffect(() => {
+    console.log('useEffect called');
+  });
+  console.log('component rendered');
+
+  return (
+    <>
+      <h1>{value}</h1>
+      <button className="btn" onClick={() => setValue(value + 1)}>
+        Change Value
+      </button>
+    </>
+  );
 };
 
 export default UseEffectBasics;
 
-// UseEffect is allows you to do the side effect
-// side effect is any work outside of the component
-// which could be signing up for subscription, fetching data, setting up an eventListener
-// just like useState, you will need to import useEffect
-// by default, useEffect will run after every re-render of a component
+// a way to use useEffect is to use a callback function in its parameters
+// this callback function will run AFTER every render
+// in this example, we will try to change the title's value
+// create an UseState hook which has a default value of zero
+// in the return, setup a button with an onCLick handler that will increase the value by one
+
+// results:
+// every time the button is clicked, you invoke the state function (setValue)
+// what will happen with the useState? 2 things happen
+// 1) preserve the value between the renders
+// 2) it triggered re-render; every time you click on the button, useEffect will run after
+
+// side note: in index.js, within the <React.StrictMode>, your App component will render twice; if placed outside, it will run once
