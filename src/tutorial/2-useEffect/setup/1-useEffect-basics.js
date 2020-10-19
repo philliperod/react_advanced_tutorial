6b474461d37b68775ddb7522c9f00d941345f3ba
Phillipe Rodriguez <philliperod@gmail.com>
2020-10-19 15:26:04 -0500

    created an if statement within the useEffect to change the value after the hook is rendered; conditionals have to be within a hook and not a hook within a conditional; the useEffect will add a string into the document title on the page after the condition is met and after everything has been rendered

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -2,9 +2,13 @@ import React, { useState, useEffect } from 'react';
 
 const UseEffectBasics = () => {
   const [value, setValue] = useState(0);
+
   useEffect(() => {
     console.log('useEffect called');
-  });
+    if (value > 1) {
+      document.title = `New Messages(${value})`;
+    }
+  }, [value]);
   console.log('component rendered');
 
   return (
@@ -19,16 +23,6 @@ const UseEffectBasics = () => {
 
 export default UseEffectBasics;
 
-// a way to use useEffect is to use a callback function in its parameters
-// this callback function will run AFTER every render
-// in this example, we will try to change the title's value
-// create an UseState hook which has a default value of zero
-// in the return, setup a button with an onCLick handler that will increase the value by one
-
-// results:
-// every time the button is clicked, you invoke the state function (setValue)
-// what will happen with the useState? 2 things happen
-// 1) preserve the value between the renders
-// 2) it triggered re-render; every time you click on the button, useEffect will run after
-
-// side note: in index.js, within the <React.StrictMode>, your App component will render twice; if placed outside, it will run once
+// Second parameters in useEffect
+// second parameter will equal an empty array
+// what will happen is the useEffect will only render initially and not every time the component runs
