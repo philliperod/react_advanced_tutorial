bc0a999eef8f4899ccce7a106efb607ce15fc1aa
Phillipe Rodriguez <philliperod@gmail.com>
2020-10-20 13:06:49 -0500

    multiple returns: setup multiple useState hooks and setup multiple conditions with a default return; two of the hooks have a default boolean value and the last hook's default value is a string and will be the default return

--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -40,19 +40,3 @@ const UseEffectFetchData = () => {
 };
 
 export default UseEffectFetchData;
-
-// UseEffectFetchData is the component which contains two hooks (useState and useEffect), async function, and returns jsx
-// useState hook returns a state value and a state function that updates which will be the users
-// state value (users) will equal the initial state (which will be disregarded afterwards) during the initial render
-// state function (setUsers) will update that state value as it accepts a new state value and queues a re-render
-// during subsequent re-renders, setUsers will always be the most recent update after applying updates
-// async function is a function that always return a promise
-// keyword (await) makes Javascript wait until that promise is settles and returns its results
-// basically, await will suspend the async function until it receives the data from the url which is stored in variable response
-// then response is resolved by json in an array and stored into users
-// users will be set in the parameters of the state fucntion setUsers
-// useEffect will run after the render is commited to the screen meaning after every completed render unless otherwise
-// useEffect will have a clean-up function that will run before the component is removed from the UI to prevent memory leaks
-//  if a component renders multiple times (as they typically do), the previous effect is cleaned up before executing the next effect
-// in the second argument, you can pass an empty array; this tells React that your effect doesn’t depend on any values from props or state, so it never needs to re-run
-// if you pass an empty array, the props and state inside the effect will always have their initial values.
--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,35 @@
 import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
 const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [user, setUser] = useState('default user');
+
+  if (!isLoading) {
+    return (
+      <div>
+        <h1>loading...</h1>
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div>
+        <h1>error...</h1>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h1>{user}</h1>
+    </div>
+  );
 };
 
 export default MultipleReturns;
+
+// common practice for naming conventions for boolean is to use 'is' first before naming
+// in this example, we setup multiple returns that have conditions
+// it will both conditions do not meet, then it will run the default return in this component
