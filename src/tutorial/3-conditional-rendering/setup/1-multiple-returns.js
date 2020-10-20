f26c0b5fffa0aa930d5ccf8f723f92dee7585f1e
Phillipe Rodriguez <philliperod@gmail.com>
2020-10-20 13:36:54 -0500

    created useEffect hook that will have a fetch request that controls the conditions; first, it will fetch the data from an url, parse it into JSON, console.log that data, and catch it for any errors; empty array in the second argument in the hook that will clean it up so it doesn't re-render

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -5,6 +5,13 @@ const MultipleReturns = () => {
   const [isError, setIsError] = useState(false);
   const [user, setUser] = useState('default user');
 
+  useEffect(() => {
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((user) => console.log(user))
+      .catch((error) => console.log(error));
+  }, []);
+
   if (!isLoading) {
     return (
       <div>
@@ -30,6 +37,8 @@ const MultipleReturns = () => {
 
 export default MultipleReturns;
 
-// common practice for naming conventions for boolean is to use 'is' first before naming
-// in this example, we setup multiple returns that have conditions
-// it will both conditions do not meet, then it will run the default return in this component
+// next example: setting up a fetch request that will control the conditions in useEffect hook
+// fetch(url) sends a GET request and returns a promise with the contents of the url
+// then((resp) => resp.json()) will take that content and parse it as JSON
+// catch((error) => console.log(error)) is a rejection handler; if any of the promises rejects then this will catch it
+// the second argument in the useEffect will clean it up; tells React that your effect doesn’t depend on any values from props or state, so it never needs to re-run
