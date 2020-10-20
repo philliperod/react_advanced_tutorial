c16d9795b2a9af699ee3476ad776bdc3e4d600d3
Phillipe Rodriguez <philliperod@gmail.com>
2020-10-20 09:38:02 -0500

    useEffect-fetch-data: setup useState, useEffect, and an async function to fetch data from an url; console.log inside the async function to showcase the objects that we will be working with

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -10,13 +10,14 @@ const UseEffectCleanup = () => {
   };
 
   useEffect(() => {
-    console.log('triggering a re-render');
+    console.log('useEffect');
     window.addEventListener('resize', checkSize);
-    return () => {
-      console.log('clean-up function working');
-      window.removeEventListener('resize', setSize);
-    };
-  });
+    // return () => {
+    //   console.log('clean-up function working');
+    //   window.removeEventListener('resize', setSize);
+    // };
+  }, []);
+  console.log('outside render');
 
   return (
     <>
@@ -27,15 +28,3 @@ const UseEffectCleanup = () => {
 };
 
 export default UseEffectCleanup;
-
-// with its current state, it will experience memory leaks (check the event Listeners in the browser)
-// so we need to create something that once we exist the hook, it will remove the event listener
-// why is this happening?
-// the callback function (state function) in checkSize is triggering the re-rendering because the moment we invoke the callback function (state function) it updates the state value
-// the useState is preserving the state value and triggering the re-rendering
-// each time we invoke the checSize function for every resizing we are also triggering the re-render
-
-// to counter the problem of this memory leak, we will use a clean-up function
-// so every time we have an useEffect hook, we can have an option of returning a function
-// the clean-up function will be invoked once it exists out of the useEffect
-//
--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -3,7 +3,36 @@ import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users';
 
 const UseEffectFetchData = () => {
-  return <h2>fetch data</h2>;
+  const [users, setUsers] = useState([]);
+  const getUsers = async () => {
+    const response = await fetch(url);
+    const users = await response.json();
+    console.log(users);
+  };
+
+  useEffect(() => {
+    getUsers();
+    // return () => {
+    //   cleanup
+    // }
+    // }, [input])
+  });
+  return (
+    <>
+      <h3>github users</h3>
+    </>
+  );
 };
 
 export default UseEffectFetchData;
+
+// 1) setup useState
+// 2) setup useEffect; in the callback function you want to perform a fetch request
+// you can setup the fetch request as a standalone or create another function
+// REMINDER: you cannot make this function async await because it returns a promise; useEffect is looking for a cleanup function and promise does that fulfill that
+// you can setup async await within the function inside the call back or write it as a separate function
+// 3) create async await as a separate function
+// 4) create sync function and within the body, we will look for a response in which you will use fetch and pass the string of the url variable
+// 5) you need to run json due to format of the url
+// 6) create users that will equal to a response of json
+// 7) invoke the getUsers async function to see the objects
