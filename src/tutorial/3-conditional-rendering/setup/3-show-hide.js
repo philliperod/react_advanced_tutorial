a3c42b3c9e30f8b513bebb355808de0db1fa4ba7
Phillipe Rodriguez <philliperod@gmail.com>
2020-10-21 10:18:34 -0500

    show/hide: setup two components (ShowHide and Item); within ShowHide component, created an useState hook with a default boolean value; created jsx with a button that will toggle that state value between false and true; in the jsc of ShowHide, create an object with an AND operator that will either display the default state value or the second component and its content

--- src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,29 @@
 import React, { useState, useEffect } from 'react';
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
+  const [show, setShow] = useState(false);
+  return (
+    <>
+      <button className="btn" onClick={() => setShow(!show)}>
+        show/hide
+      </button>
+      {show && <Item />}
+    </>
+  );
+};
+
+const Item = () => {
+  return (
+    <div style={{ marginTop: '2rem' }}>
+      <h1>window</h1>
+      <h2>size: </h2>
+    </div>
+  );
 };
 
 export default ShowHide;
+
+// create a hook that has a boolean for a default value
+// create your jsx where a button will toggle the state boolean value
+// create another component that will display its own jsx
+// in the ShowHide component, setup a condition that will toggle between the first argument (state value) and second argument (Item component and its contents)
