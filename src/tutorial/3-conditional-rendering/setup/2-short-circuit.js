4c85018bc919423f9d5f0af104c6ebfefda4a60e
Phillipe Rodriguez <philliperod@gmail.com>
2020-10-21 08:32:44 -0500

    used the OR and AND operator in JSX to show an alternate option rather than setting up a variable with that logic

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,14 +1,16 @@
 import React, { useState } from 'react';
 
 const ShortCircuit = () => {
-  const [text, setText] = useState('k');
+  const [text, setText] = useState('');
   const firstValue = text || 'hello world';
   const secondValue = text && 'hello world';
 
   return (
     <>
-      <h1>{firstValue}</h1>
-      <h2>value: {secondValue}</h2>
+      {/* <h1>{firstValue}</h1>
+      <h2>value: {secondValue}</h2> */}
+      <h1>{text || 'Phil Rod'}</h1>
+      {text && <h2>'Rod Phil</h2>}
     </>
   );
 };
@@ -18,5 +20,8 @@ export default ShortCircuit;
 // short-circuit evaluation
 // ternary operator
 
-// firstValue returns second argument because the first argument is false (empty string from the useState default value)
-// secondValue returns nothing as both arguments are not true
+// how to use this in JSX?
+// you can use the OR and AND operator within the object in JSX in which you can setup a default value
+// since the default value is an empty string, it is a falsy statement
+// the first one will return the default value
+// the second one will return the first value (empty string)
