9eaa3f091e63960f3a31290ef16c096a1af6bfd4
Phillipe Rodriguez <philliperod@gmail.com>
2020-10-28 18:10:43 -0400

    created a function to handle the submission; invoke the preventDefault() function to prevent the browser to submitting rather than the form

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
-// JS
-// const input = document.getElementById('myText');
-// const inputValue = input.value
-// React
-// value, onChange
 
 const ControlledInputs = () => {
+  const handleSubmit = (submission) => {
+    submission.preventDefault();
+    console.log('hello everybody');
+  };
+
   return (
     <>
       <article>
-        <form className="form">
+        <form className="form" onSubmit={handleSubmit}>
           <div className="form-control">
             <label htmlFor="firstName">Name: </label>
             <input type="text" name="firstName" id="firstName" />
@@ -27,4 +27,9 @@ const ControlledInputs = () => {
 
 export default ControlledInputs;
 
-//
+// there are two ways to have a submit
+// 1) add onSubmit onto the main form
+// 2) add onClick onto the <button type='submit'>
+// now what will happen when you press enter or click on submit?
+// Your browser will, first, attempt to submit the form which will reset the form
+// 3) object.preventDefault(): prevents the browser from submitting the form rather than submitting through your function
